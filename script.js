f22ae9e3569c961a0b6ae3c4ab09ac5c301f67a2
codewithrafiq <codewithrafiq@gmail.com>
2023-02-24 19:15:02 +0600

    class 11

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-// function a() {
-//     console.log(b);
+// // function a() {
+// //     console.log(b);
+// // }
+
+// // var b = 10;
+
+// // a();
+
+// // console.log(a);
+
+// // let a = 10;
+// // // var a = 10;
+// // const b=20
+
+// // var p =49;
+// // console.log(this.p)
+
+// var c = "ccc";
+// {
+//   let a = "a";
+//   const b = "b";
+//   var c = "c";
 // }
 
-// var b = 10;
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
 
-// a();
+// function y(){
+//     for(var i=1;i<=5;i++){
+//         setTimeout(function(){
+//             console.log(i);
+//         },i*1000);
+//     }
+//     console.log("Namaster JS");
+// }
+// y();
+// function y(){
+//     for(let i=1;i<=5;i++){
+//         setTimeout(function(){
+//             console.log(i);
+//         },i*1000);
+//     }
+//     console.log("Namaster JS");
+// }
+// y();
 
-
-// console.log(a);
-
-
-// let a = 10;
-// // var a = 10;
-// const b=20
-
-// var p =49;
-// console.log(this.p)
-
-
+function y() {
+  for (var i = 1; i <= 5; i++) {
+    function close(z) {
+      setTimeout(function () {
+        console.log(z);
+      }, z * 1000);
+    }
+    close(i);
+  }
+  console.log("Namaster JS");
+}
+y();
